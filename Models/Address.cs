a803a27177e77ebcdc5d90a9dbdf293facec3f2a
Admin <Admin@KOMP_KOZACKI_ES>
2021-03-15 19:59:23 +0100

    Sln

---
+++ Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zaj2.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica"),StringLength(60, MinimumLength = 3,ErrorMessage = "Pole Street musi zawierać od 3 do 60 znaków"),Required(ErrorMessage = "Pole Street jest obowiązkowe")]
+        public string Street { get; set; }
+        [Required(ErrorMessage = "Pole ZipCode jest obowiązkowe")]
+        public string ZipCode { get; set; }
+        [Required(ErrorMessage ="Pole City jest obowiązkowe")]
+        public string City { get; set; }
+        [Required]
+        public int Number { get; set; }
+
+    }
+}
